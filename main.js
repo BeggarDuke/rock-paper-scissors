55920bdadbe3461311387d2daec3e45c8e31b2a0
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2022-12-21 19:56:40 +0300

    feat: add new gamemode - 5 games in a row
    Game function has also been edited for this gamemode - now game function
    give value for "choose" variables while game started, these variables no
    longer take on a value before game started. It is better for the order
    execution of functions and overall code.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+let playerChoice;
+let computerChoice;
+let playerScore = 0;
+let computerScore = 0;
+
 function computerSelection() {
     let choice = Math.floor(Math.random() * 3);
     return (choice === 0) ? "rock" :
@@ -23,13 +28,9 @@ function playerSelection() {
         }
     }
 }
-let playerChoice = playerSelection();
-let computerChoice = computerSelection();
-let playerWin = 0;
-let computerWin = 0;
 
-function game(playerChoice, computerChoice) {
-
+function game() {
+    selectChoices();
     if (playerChoice === computerChoice) {
         alert("Draw!");
     }
@@ -37,36 +38,72 @@ function game(playerChoice, computerChoice) {
     else {
         switch (playerChoice + " " + computerChoice) {
             case "rock scissors":
-                playerWin++;
+                ++playerScore;
                 alert("You won! Rock beats scissors!");
                 break;
             
             case "rock paper":
-                computerWin++
+                ++computerScore;
                 alert("You lose! Paper beats rock!");
                 break;
 
             case "paper rock":
-                playerWin++;
+                ++playerScore;
                 alert("You won! Paper beats rock!");
                 break;
             
             case "paper scissors":
-                computerWin++;
+                ++computerScore;
                 alert("You lose! Scissors beats paper!");
                 break;  
 
             case "scissors paper":
-                playerWin++;
+                ++playerScore;
                 alert("You won! Scissors beats paper!");
                 break;
             
             case "scissors rock":
-                computerWin++;
+                ++computerScore;
                 alert("You lose! Rock beats scissors!");
                 break;
         }
     }
 }
-    
-game(playerChoice, computerChoice);
+
+function selectChoices () {
+    playerChoice = playerSelection();
+    computerChoice = computerSelection();
+}
+
+function game5 () {
+    for (i = 0; i < 5; i++) {
+        game();
+        console.log(`Player score: ${playerScore}, Computer score: ${computerScore}`)
+    }
+    if (playerScore === computerScore) {
+        alert(`
+        Draw! 
+        Player score: ${playerScore} 
+        Computer score: ${computerScore}
+        `)
+    }
+    else if (playerScore > computerScore) {
+        alert(`
+        You won! 
+        Player score: ${playerScore} 
+        Computer score: ${computerScore}
+        `)
+    }
+    else {
+        alert(`
+        You lose! 
+        Player score: ${playerScore}
+        Computer score: ${computerScore}
+        `)
+    }
+}
+
+let gameMode = prompt("Type \"1\" to play just 1 game, type \"5\" to play 5 games in a row");
+gameMode === "5" ? game5() :
+gameMode === "1" ? game() :
+alert("You need to choose gamemode");
